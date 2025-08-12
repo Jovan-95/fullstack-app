236c364887154126c5c459efc46063df4058a27f
Boris <bbogdanovic167@gmail.com>
2025-08-12 15:51:40 +0200

    login and logout bekend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,41 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginUserRequest;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
-   public function getAdmin()
-    {
-        
-        $admin = User::where('is_admin', true)->first();
+public function login(LoginUserRequest $request)
+{
+    if (Auth::attempt($request->only('email', 'password'))) {
+        $user = Auth::user();
 
-        if (!$admin) {
-            return response()->json(['message' => 'Admin not found'], 404);
+        if ($user) {
+            return response()->json([
+            'status' => true,
+            'message' => 'Welcome ' . $user->name . ' ' . $user->last_name,
+            'data' => $user,
+            'auth_token' => $user->createToken('auth_token')->plainTextToken,
+        ], 200);
         }
+}
+return response()->json([
+        'message' => 'The provided credentials are incorrect.',
+    ], 401);
+}
 
-        return response()->json($admin);
-    }
+
+public function logout(){
+Auth::user()->currentAccessToken()->delete();
+
+    return response()->json([
+        'Message' => 'The user has been successfully logged out'
+    ], 200);
+
+}
+
+
 }
