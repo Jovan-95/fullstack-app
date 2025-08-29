7d1aa5b3311f54d1a16905415bb87f46ca5f4f4a
Boris <bbogdanovic167@gmail.com>
2025-08-29 10:29:06 +0200

    fixing update image

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\UserController;
 
 
 ///////////////////////////////////////////AUTH ROUTES
-
 Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->middleware('throttle:5,1');
 Route::post('forgot-password', [\App\Http\Controllers\AuthController::class, 'forgot']);
@@ -14,7 +13,6 @@ Route::post('reset-password', [\App\Http\Controllers\AuthController::class, 'res
 Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
 
 ///////////////////////////////////////////USERS ROUTES
-
 Route::get('users', [\App\Http\Controllers\UserController::class, 'getUsers'])->middleware('auth:sanctum');
 Route::patch('user/settings', [\App\Http\Controllers\UserController::class, 'updateProfile'])->middleware('auth:sanctum');
-Route::post('user/avatar', [\App\Http\Controllers\UserController::class, 'updateAvatar'])->middleware('auth:sanctum');
+Route::patch('user/avatar', [\App\Http\Controllers\UserController::class, 'updateAvatar'])->middleware('auth:sanctum');
